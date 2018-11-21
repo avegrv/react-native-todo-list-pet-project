65cc581605256d425e99a92376f642ce1ab4c6f5
Alexandr Egorov <avegrv@gmail.com>
2018-11-22 00:15:09 +0400

    Implemented completed project

--- app/containers/App.js
+++ app/containers/App.js
@@ -6,16 +6,23 @@ import {actionCreators} from '../redux/todoRedux'
 import Title from "../components/Title";
 import SafeAreaView from "react-native-safe-area-view";
 import Footer from "../components/Footer";
+import Input from "../components/Input";
+import List from "../components/List";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    divider: {
+        height: 1,
+        backgroundColor: 'whitesmoke'
+    }
 })
 
 const mapStateToProps = (state) => ({
     items: state.items,
 })
+
 
 class App extends Component {
 
@@ -24,10 +31,43 @@ class App extends Component {
         dispatch: PropTypes.func.isRequired,
     }
 
+    addItem = (item) => {
+        const {dispatch} = this.props
+        dispatch(actionCreators.addItem(item))
+    }
+
+    removeItem = (index) => {
+        const {dispatch} = this.props
+        dispatch(actionCreators.removeItem(index))
+    }
+
+    toggleItemCompleted = (index) => {
+        const {dispatch} = this.props
+        dispatch(actionCreators.toggleItemCompleted(index))
+    }
+
+    removeCompleted = () => {
+        const {dispatch} = this.props
+        dispatch(actionCreators.removeCompleted())
+    }
+
     render() {
+        const {items} = this.props
+
         return <SafeAreaView style={styles.container}>
-            <Title> Todo list </Title>
-            <Footer></Footer>
+            <Title> Todo List </Title>
+            <Input
+                placeholder={'Enter an item!'}
+                onSubmit={this.addItem}
+            />
+            <View style={styles.divider}/>
+            <List
+                items={items}
+                onRemoveItem={this.removeItem}
+                onToggleItemCompleted={this.toggleItemCompleted}
+            />
+            <View style={styles.divider} />
+            <Footer onRemoveCompleted={this.removeCompleted} />
         </SafeAreaView>
     }
 }
--- app/redux/todoRedux.js
+++ app/redux/todoRedux.js
@@ -1,6 +1,24 @@
-const types = {}
+const types = {
+    ADD_ITEM: 'ADD_ITEM',
+    REMOVE_ITEM: 'REMOVE_ITEM',
+    TOGGLE_ITEM_COMPLETED: 'TOGGLE_ITEM_COMPLETED',
+    REMOVE_COMPLETED: 'REMOVE_COMPLETED',
+}
 
-export const actionCreators = {}
+export const actionCreators = {
+    addItem: (item) => {
+        return {type: types.ADD_ITEM, payload: item}
+    },
+    removeItem: (index) => {
+        return {type: types.REMOVE_ITEM, payload: index}
+    },
+    toggleItemCompleted: (index) => {
+        return {type: types.TOGGLE_ITEM_COMPLETED, payload: index}
+    },
+    removeCompleted: (item) => {
+        return {type: types.REMOVE_COMPLETED, payload: item}
+    },
+}
 
 const initialState = {
     items: [],
@@ -8,8 +26,36 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     const {type, payload} = action
+    const {items} = state
 
-    switch (type) {
+    switch(type) {
+        case types.ADD_ITEM: {
+            return {
+                ...state,
+                items: [{label: payload, completed: false}, ...items],
+            }
+        }
+        case types.REMOVE_ITEM: {
+            return {
+                ...state,
+                items: items.filter((item, i) => i !== payload),
+            }
+        }
+        case types.TOGGLE_ITEM_COMPLETED: {
+            return {
+                items: items.map((item, i) => {
+                    if (i === payload) {
+                        return {label: item.label, completed: !item.completed}
+                    }
+                    return item
+                }),
+            }
+        }
+        case types.REMOVE_COMPLETED: {
+            return {
+                items: items.filter((item) => !item.completed)
+            }
+        }
         default: {
             return state
         }
